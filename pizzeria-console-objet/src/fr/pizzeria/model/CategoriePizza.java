5ea92d9913b623dbfe00076376cd8d26acab61ab
Kevin Mokili <kevin.mokili@gmail.com>
2016-04-29 14:10:45 +0200

    Création type énuméré CategoriePizza

---
+++ pizzeria-console-objet/src/fr/pizzeria/model/CategoriePizza.java
@@ -0,0 +1,12 @@
+package fr.pizzeria.model;
+
+public enum CategoriePizza {
+	VIANDE ("Viande"), POISSON("Poisson"), SANS_VIANDE("Sans Viande");
+	
+	private String libelle;
+
+	private CategoriePizza(String libelle)
+	{
+		this.libelle = libelle;
+	}
+}
