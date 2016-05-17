9f966bd3e4f66cec17b55f354b6242f56b25c976
Kevin Mokili <kevin.mokili@gmail.com>
2016-05-17 11:15:05 +0200

    Annotation @Entity sur classe Commande

--- pizzeria-modele/src/main/java/fr/pizzeria/model/Commande.java
+++ pizzeria-modele/src/main/java/fr/pizzeria/model/Commande.java
@@ -2,12 +2,14 @@ package fr.pizzeria.model;
 
 import java.sql.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Commande {
 
 	
