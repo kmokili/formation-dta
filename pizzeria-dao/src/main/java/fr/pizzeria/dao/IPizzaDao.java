5666affff071489d3665d7275adcd89abcdf27bb
Kevin Mokili <kevin.mokili@gmail.com>
2016-05-26 10:26:08 +0200

    changement propriété failOnMissingWebXml à false

--- pizzeria-dao/src/main/java/fr/pizzeria/dao/IPizzaDao.java
+++ pizzeria-dao/src/main/java/fr/pizzeria/dao/IPizzaDao.java
@@ -7,6 +7,7 @@ import fr.pizzeria.exception.DaoException;
 import fr.pizzeria.model.Pizza;
 
 public interface IPizzaDao {
+	IPizzaDao DEFAULT_IMPLEMENTATION = new PizzaDaoImpl();
 	
 	List<Pizza> findAllPizzas() throws DaoException;
 	void savePizza(Pizza newPizza) throws DaoException;
