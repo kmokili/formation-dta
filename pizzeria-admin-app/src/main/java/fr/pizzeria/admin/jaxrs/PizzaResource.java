768d491d70809e0a562d2302c5841d0a76f18c11
Kevin Mokili <kevin.mokili@gmail.com>
2016-05-27 15:23:45 +0200

    correction méthodes {update, delete}Pizza de PizzaService

--- pizzeria-admin-app/src/main/java/fr/pizzeria/admin/jaxrs/PizzaResource.java
+++ pizzeria-admin-app/src/main/java/fr/pizzeria/admin/jaxrs/PizzaResource.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -41,14 +43,15 @@ public class PizzaResource {
 	}
 	
 	@PUT
-	@Produces(MediaType.APPLICATION_JSON)
-	public void updatePizza(String codePizza, Pizza updatePizza) throws DaoException {
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Pizza updatePizza(String codePizza, Pizza updatePizza) throws DaoException {
 		pizzaService.updatePizza(codePizza, updatePizza);
+		return updatePizza;
 	}
 	
 	@DELETE
-	@Produces(MediaType.APPLICATION_JSON)
-	public void deletePizza(String codePizza) throws DaoException {
+	@Path("/{code}")
+	public void deletePizza(@PathParam("code") String codePizza) throws DaoException {
 		pizzaService.deletePizza(codePizza);
 	}
 	
--- pizzeria-admin-app/src/main/java/fr/pizzeria/admin/metier/PizzaService.java
+++ pizzeria-admin-app/src/main/java/fr/pizzeria/admin/metier/PizzaService.java
@@ -49,7 +49,9 @@ public class PizzaService implements Serializable{
 	
 	public void updatePizza(String codePizza, Pizza updatePizza) throws DaoException {
 		TypedQuery<Pizza> query = 
-				em.createQuery("select p from Pizza p where p.code='codePizza'", Pizza.class);
+				em.createQuery("select p from Pizza p where p.code=:codePizza", Pizza.class)
+				.setParameter("codePizza", codePizza);
+		
 		Pizza p = query.getResultList().get(0);
 		
 		p.setCode(updatePizza.getCode());
@@ -61,7 +63,8 @@ public class PizzaService implements Serializable{
 	
 	public void deletePizza(String codePizza) throws DaoException {
 		TypedQuery<Pizza> query = 
-				em.createQuery("delete p from Pizza p where p.code='codePizza'", Pizza.class);
+				em.createQuery("delete p from Pizza p where p.code=:codePizza", Pizza.class)
+				.setParameter("codePizza", codePizza);
 		Pizza p = query.getResultList().get(0);
 		
 		if (p != null) {
