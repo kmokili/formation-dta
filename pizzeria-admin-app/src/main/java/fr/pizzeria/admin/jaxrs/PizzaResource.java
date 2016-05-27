e7ba032ff1b8acf0e8624e9e7a0627fb657789fa
Kevin Mokili <kevin.mokili@gmail.com>
2016-05-27 12:21:51 +0200

    dédut migration JPA > JAX-RS

---
+++ pizzeria-admin-app/src/main/java/fr/pizzeria/admin/jaxrs/PizzaResource.java
@@ -0,0 +1,45 @@
+package fr.pizzeria.admin.jaxrs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import fr.pizzeria.exception.DaoException;
+import fr.pizzeria.model.Pizza;
+
+@Path("/pizzas")
+public class PizzaResource {
+
+	public PizzaResource() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Pizza> findAllPizzas() throws DaoException {
+		// pizze : pluriel de pizza (italien)
+		List<Pizza> pizze = new ArrayList<Pizza>();
+		return pizze;
+		
+	}
+	
+	public void savePizza(Pizza newPizza) throws DaoException {
+		
+	}
+	
+	
+	public void updatePizza(String codePizza, Pizza updatePizza) throws DaoException {
+		
+	}
+	
+	public void deletePizza(String codePizza) throws DaoException {
+		
+	}
+	
+	
+}
