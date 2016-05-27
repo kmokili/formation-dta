a27292d11bffd8d4f53d2435e5638d29da6cbbde
Kevin Mokili <kevin.mokili@gmail.com>
2016-05-27 12:30:13 +0200

    initialisation base de données pizzeria por drop and create

--- pizzeria-admin-app/src/main/java/fr/pizzeria/admin/jaxrs/PizzaResource.java
+++ pizzeria-admin-app/src/main/java/fr/pizzeria/admin/jaxrs/PizzaResource.java
@@ -3,17 +3,20 @@ package fr.pizzeria.admin.jaxrs;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.TypedQuery;
+import javax.inject.Inject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import fr.pizzeria.admin.metier.PizzaService;
 import fr.pizzeria.exception.DaoException;
 import fr.pizzeria.model.Pizza;
 
 @Path("/pizzas")
 public class PizzaResource {
+	@Inject private PizzaService pizzaService;
 
 	public PizzaResource() {
 		// TODO Auto-generated constructor stub
