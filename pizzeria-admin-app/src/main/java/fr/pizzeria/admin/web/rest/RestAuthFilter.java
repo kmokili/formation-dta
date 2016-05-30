8c0de2969c488a8673a01ebab9857d1b0cdee583
Kevin Mokili <kevin.mokili@gmail.com>
2016-05-30 11:44:35 +0200

    ajout Login, Token AuthFilter

---
+++ pizzeria-admin-app/src/main/java/fr/pizzeria/admin/web/rest/RestAuthFilter.java
@@ -0,0 +1,31 @@
+package fr.pizzeria.admin.web.rest;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+public class RestAuthFilter implements ContainerRequestFilter {
+
+	@Inject private TokenService tokenService;
+	
+
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		String token = requestContext.getHeaderString("auth");
+
+			if(!tokenService.isTokenValid(token)
+					&& !requestContext.getUriInfo().getPath().contains("/login")) {
+				
+				// non authentifié
+				requestContext.abortWith(Response.status(Status.FORBIDDEN).build());
+			}
+		}
+		
+	
+
+}
---
+++ pizzeria-admin-app/src/main/java/fr/pizzeria/admin/web/rest/TokenService.java
@@ -0,0 +1,24 @@
+package fr.pizzeria.admin.web.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TokenService {
+
+	private List<String> tokensValides = new ArrayList<String>();
+	
+	public String generateNewToken() {
+		String token = UUID.randomUUID().toString();
+		tokensValides.add(token);
+		return token;
+	}
+
+	public boolean isTokenValid(String token) {
+		return tokensValides.contains(token);
+	}
+	
+}
