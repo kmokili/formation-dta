b04b59419c3bf6977980adfb80ed75b095fc73de
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-03 17:05:22 +0200

    commit

--- pizzeria-console-objet-java8/src/main/java/fr/pizzeria/console/PizzeriaAppSpringConfig.java
+++ pizzeria-console-objet-java8/src/main/java/fr/pizzeria/console/PizzeriaAppSpringConfig.java
@@ -13,11 +13,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
