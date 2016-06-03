b415bdefc11608707fe09f2a5e2cfc7ea31a8d50
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-03 17:10:05 +0200

    HelloWorldController

---
+++ pizzeria-spring-web/src/main/java/fr/pizzeria/spring/mvc/controller/HelloWorldController.java
@@ -0,0 +1,10 @@
+package fr.pizzeria.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HelloWorldController {
+
+}
