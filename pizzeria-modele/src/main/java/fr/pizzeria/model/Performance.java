e983a9efbb06fb1974ea1dad74a3cb5535e332fa
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-06 16:28:37 +0200

    Modifications
    * Retrait classe SecurityConfig
    * Ajout dépendance junit dans pom pizzeria-dao
    * Ajout module pizzeria-spring-web dans pom pizzeria-parent

--- pizzeria-modele/src/main/java/fr/pizzeria/model/Performance.java
+++ pizzeria-modele/src/main/java/fr/pizzeria/model/Performance.java
@@ -2,17 +2,21 @@ package fr.pizzeria.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="performance")
 public class Performance {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String service;
 	private Date date;
+	@Column(name="temps_execution")
 	private Long tempsExecution;
 	
 	
